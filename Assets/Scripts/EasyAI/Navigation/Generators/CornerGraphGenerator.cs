5b45a797fd54e1d5d5f51e3c5a151328eb5d1f18
Adam Taylor <adam.taylor0147@gmail.com>
2023-03-30 16:21:52 -0400

    A4

--- Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
+++ Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using EasyAI.Navigation.Nodes;
+using UnityEngine;
 
 namespace EasyAI.Navigation.Generators
 {
@@ -11,13 +12,28 @@ namespace EasyAI.Navigation.Generators
         [Min(0)]
         [Tooltip("How far away from corners should the nodes be placed.")]
         private int cornerNodeSteps = 3;
-    
+
+       
+     
         /// <summary>
         /// Place nodes at convex corners.
         /// </summary>
         public override void Generate()
         {
             // TODO - Assignment 4 - Complete corner-graph node generation.
+            
+            for(int i = 0; i < NodeArea.RangeX; i++) 
+            {
+                for(int k = 0; k < NodeArea.RangeZ; k++) 
+                {
+                    if (NodeArea.IsOpen(i, k) == true)
+                    {
+                        NodeArea.AddNode(i, k);
+                    }
+                }
+            }
+            
+            
         }
     }
 }
